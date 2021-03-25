1eb54c040dd43dda1182947c16de489e9fdad39b
hustf <hustf@users.noreply.github.com>
2021-03-25 17:08:02 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/MechGlueInterpolations.jl
@@ -0,0 +1,5 @@
+module MechGlueInterpolations
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MechGlueInterpolations
+using Test
+
+@testset "MechGlueInterpolations.jl" begin
+    # Write your tests here.
+end
